07aecc19a1192a72eb3f098f0b9bf4ea24367524
Relief Melone <relief.melone@gmail.com>
2022-12-29 10:59:47 +0100

    day14 complete

--- day_14/src/lib.rs
+++ day_14/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, time::Instant};
+use std::{collections::HashMap, time::Instant };
 
 use input_reader::read_lines;
 mod input_reader;
@@ -13,16 +13,17 @@ enum FieldType {
 struct Grid {
     grid: HashMap<(usize, usize), FieldType>,
     moving_particles: Vec<(usize, usize)>,
-    sand_source: (usize, usize),
+    sand_source: (usize, usize),    
     min_x: usize,
     max_x: usize,
-    max_y: usize
+    max_y: usize,
+    has_floor: bool
 }
 
 pub fn part_01(path: Option<&str>) -> i32 {
     let started = Instant::now();
     let input = read_lines(path.unwrap_or("assets/input.txt"));
-    let mut grid = Grid::new(input);
+    let mut grid = Grid::new(input,false);
     let mut resting_grains_count = 0;
     let mut grain_comes_to_rest: Result<(),()> = Ok(());
 
@@ -35,11 +36,28 @@ pub fn part_01(path: Option<&str>) -> i32 {
     println!("Execution time for part 1: {:?}", Instant::now() - started);    
 
     resting_grains_count
-    
+}
+
+pub fn part_02(path: Option<&str>) -> i32 {
+    let started = Instant::now();
+    let input = read_lines(path.unwrap_or("assets/input.txt"));
+    let mut grid = Grid::new(input, true);
+    let mut resting_grains_count = 0;
+    let mut grain_comes_to_rest: Result<(),()> = Ok(());
+
+    while let Ok(()) = grain_comes_to_rest {
+        grain_comes_to_rest = grid.run_single_sand_grain();
+        resting_grains_count += 1; 
+    }
+
+    println!("Part 02 - Grains that can be processed: {}", resting_grains_count);
+    println!("Execution time for part 2: {:?}", Instant::now() - started);    
+    // grid.draw();
+    resting_grains_count
 }
 
 impl Grid {
-    fn new(input: Vec<String>)-> Self {
+    fn new(input: Vec<String>, has_floor:bool)-> Self {
 
         let mut grid:HashMap<(usize,usize), FieldType> = HashMap::new();
         let mut max_x = 0;
@@ -85,9 +103,11 @@ impl Grid {
             max_x,
             min_x,
             max_y,
+            has_floor
         }
     }
 
+    #[allow(dead_code)]
     fn draw(&self){
         for y in 0..=(self.max_y+3) {
             let mut line = "".to_string();
@@ -102,9 +122,11 @@ impl Grid {
         }
     }
 
-
+    #[allow(dead_code, unused)]
     fn tick(&mut self) -> Result<usize, &str> {
-        let mut indices_not_moving_anymore:Vec<usize> = vec![];
+        let mut indices_not_moving_anymore:Vec<usize> = vec![];        
+        let mut max_x = self.max_x;
+        let mut min_x = self.min_x;
 
         for (index, particle) in self.moving_particles.iter_mut().enumerate() {
             let old_position = particle.clone();
@@ -120,24 +142,26 @@ impl Grid {
                 _ => *particle
             };
 
-            if old_position == *particle {
+            if old_position == *particle || (self.has_floor && particle.1 == self.max_y+1) {
                 self.grid.insert(*particle, FieldType::Sand);
                 indices_not_moving_anymore.push(index);                
+            }            
+            
+            if !self.has_floor && particle.1 > self.max_y {
+                return Err("particle will not come to rest anymore");
             }
 
-            
-            if particle.1 > self.max_y {
-                return Err("particle will not come to rest anymore");
+            if self.has_floor && *particle == (self.sand_source){
+                return Err("particle is already at sand source");
             }
         }
 
         for ind in indices_not_moving_anymore {
             self.moving_particles.remove(ind);
-        };
-
-        
+        };   
 
         Ok(self.moving_particles.len())
+
     }
 
     fn run_single_sand_grain(&mut self) -> Result<(),()>{
@@ -150,7 +174,6 @@ impl Grid {
         }
 
         Ok(())
-
     }
 }
 
@@ -161,24 +184,17 @@ mod test {
 
     use super::*;
 
-    // #[test]
-    // fn grid_new_01(){
-    //     let input = read_lines("assets/input_test_01.txt");
-    //     let mut grid = Grid::new(input);
-
-    //     grid.run_single_sand_grain().unwrap();
-    //     grid.run_single_sand_grain();
-    //     grid.run_single_sand_grain();
-    //     grid.run_single_sand_grain();
-    //     grid.run_single_sand_grain();
-    // }
-
     #[test]
     fn part01_01(){
         let grains_processed = part_01(Some("assets/input_test_01.txt"));
 
-        assert_eq!(grains_processed, 24)
+        assert_eq!(grains_processed, 24)        
+    }
 
-        
+    #[test]
+    fn part02_01(){
+        let grains_processed = part_02(Some("assets/input_test_01.txt"));
+
+        assert_eq!(grains_processed, 93)
     }
 }
--- day_14/src/main.rs
+++ day_14/src/main.rs
@@ -1,5 +1,6 @@
-use day_14::part_01;
+use day_14::{part_01, part_02};
 
 fn main() {
     part_01(None);
+    part_02(None);
 }
